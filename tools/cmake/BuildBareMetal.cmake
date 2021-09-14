561a9242afda023496505fa6e03e3a3861ea1f0a
nekosanteam <1688092+nekosanteam@users.noreply.github.com>
2021-09-15 00:25:08 +0900

    modified.

---
+++ tools/cmake/BuildBareMetal.cmake
@@ -0,0 +1,11 @@
+macro(target_compile_option_baremetal Target_)
+  target_compile_options(${Target_}
+    PRIVATE $<$<AND:$<PLATFORM_ID:Generic>,$<COMPILE_LANGUAGE:C,CXX>>:-ffreestanding>)
+endmacro()
+
+macro(target_link_option_baremetal Target_ LdScript_)
+  set_target_properties(${Target_}
+    PROPERTIES LINK_DEPENDS $<$<PLATFORM_ID:Generic>:${LdScript_}> )
+  target_link_options(${Target_}
+    PRIVATE $<$<PLATFORM_ID:Generic>:-nostdlib -T ${LdScript_}> )
+endmacro()
--- tools/cmake/aarch64-linux-gcc.cmake
+++ tools/cmake/aarch64-linux-gcc.cmake
@@ -1,5 +1,5 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 set(GCC_CROSS_COMPILE "aarch64-linux-gnu-")
-set(GCC_VERSION "")
+set(GCC_VERSION $ENV{GCC_VERSION})
 include(_gcc-common)
