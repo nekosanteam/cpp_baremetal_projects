a313aa7fda7ed7cbc5ffc3e1d5acd0c62efb3230
nekosanteam <1688092+nekosanteam@users.noreply.github.com>
2021-09-29 01:56:42 +0900

    modify.

--- tools/build_all.sh
+++ tools/build_all.sh
@@ -1,35 +1,10 @@
 #!/bin/sh
 
-export GCC_VERSION=
-export CLANG_VERSION=-13
+export GCC_VERSION=${GCC_VERSION:-}
+export GCC_SYSROOT=${GCC_SYSROOT:-/usr/aarch64-linux-gnu/}
+export CLANG_VERSION=${CLANG_VERSION:--13}
+export CLANG_SYSROOT=${CLANG_SYSROOT:-/usr/aarch64-linux-gnu/}
 mkdir -p build_all
-
-TARGET1=build_all/native_gcc
-rm -rf $TARGET1
-CC=gcc${GCC_VERSION} CXX=g++${GCC_VERSION} cmake . -B $TARGET1    || exit 1
-CC=gcc${GCC_VERSION} CXX=g++${GCC_VERSION} cmake --build $TARGET1 || exit 1
-
-TARGET2=build_all/native_clang
-rm -rf $TARGET2
-CC=clang${CLANG_VERSION} CXX=clang++${CLANG_VERSION} cmake . -B $TARGET2    || exit 1
-CC=clang${CLANG_VERSION} CXX=clang++${CLANG_VERSION} cmake --build $TARGET2 || exit 1
-
-TARGET3=build_all/aarch64_gcc
-rm -rf $TARGET3
-cmake . -B $TARGET3 -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=tools/cmake/aarch64-linux-gcc.cmake
-cmake --build $TARGET3
-
-TARGET4=build_all/aarch64_clang
-rm -rf $TARGET4
-cmake . -B $TARGET4 -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=tools/cmake/aarch64-linux-clang.cmake
-cmake --build $TARGET4
-
-TARGET5=build_all/aarch64_gcc_bare
-rm -rf $TARGET5
-cmake . -B $TARGET5  -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE=tools/cmake/aarch64-none-gcc.cmake
-cmake --build $TARGET5
-
-TARGET6=build_all/aarch64_clang_bare
-rm -rf $TARGET6
-cmake . -B $TARGET6  -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE=tools/cmake/aarch64-none-clang.cmake
-cmake --build $TARGET6
+tools/build_sh/native.sh
+tools/build_sh/aarch64.sh
+tools/build_sh/aarch64_bare.sh
