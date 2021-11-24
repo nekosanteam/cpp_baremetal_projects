cb6b11fd2bd68b8a4289a69ec216f2e154d1a840
nekosanteam <1688092+nekosanteam@users.noreply.github.com>
2021-11-25 01:21:40 +0900

    modify.

--- 00_startup/src/startup.c
+++ 00_startup/src/startup.c
@@ -3,9 +3,9 @@ void clear_bss()
     extern void* __bss_start;
     extern void* __bss_end;
 
-    unsigned char* p;
-    unsigned char* start = (unsigned char*)&__bss_start;
-    unsigned char* end   = (unsigned char*)&__bss_end;
+    unsigned int* p;
+    unsigned int* start = (unsigned char*)__bss_start;
+    unsigned int* end   = (unsigned char*)__bss_end;
 
     for (p = start; p < end; p++) {
         *p = 0;
