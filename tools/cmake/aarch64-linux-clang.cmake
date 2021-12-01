bf359968c2355e0707d8d35528fcda42cde774df
nekosanteam <1688092+nekosanteam@users.noreply.github.com>
2021-12-01 23:48:52 +0900

    cleanup directories.

--- tools/cmake/aarch64-linux-clang.cmake
+++ tools/cmake/aarch64-linux-clang.cmake
@@ -1,5 +1,5 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
-set(CLANG_TRIPLE "aarch64-linux-gnu")
+set(CLANG_TRIPLE  "aarch64-linux-gnu")
 set(CLANG_VERSION $ENV{CLANG_VERSION})
 include(_clang-common)
--- tools/cmake/aarch64-none-clang.cmake
+++ tools/cmake/aarch64-none-clang.cmake
@@ -1,7 +1,7 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
-set(CLANG_TRIPLE "aarch64-linux-gnu")
+set(CLANG_TRIPLE  "aarch64-linux-gnu")
 set(CLANG_VERSION $ENV{CLANG_VERSION})
 include(_clang-common)
 
