2699d538941da6add36b46251c6652f1a35ce9f5
nekosanteam <1688092+nekosanteam@users.noreply.github.com>
2022-04-05 00:45:13 +0900

    wip.

--- platforms/00_Startup/test/main.c
+++ platforms/00_Startup/test/main.c
@@ -2,11 +2,39 @@
 #include <stdint.h>
 
 char buffer[16];
+void put_message();
 
 int main(void)
 {
     for (int i=0; i<16; i++) {
         buffer[i] = 1;
     }
+    put_message();
     return 0;
 }
+
+#ifdef __linux
+#include <sys/syscall.h>
+unsigned int
+bm_sys_write(int fd, const char* buf, int size)
+{
+    unsigned int ret;
+    __asm volatile (
+        "syscall"
+        : "=a" (ret)
+        //                 EDI      RSI       RDX
+        : "0"(SYS_write), "D"(fd), "S"(buf), "d"(size)
+        : "rcx", "r11", "memory"
+    );
+    return ret;
+}
+
+void put_message()
+{
+    bm_sys_write(1, "hello,world\n", 13);
+}
+#else
+void put_message()
+{
+}
+#endif
