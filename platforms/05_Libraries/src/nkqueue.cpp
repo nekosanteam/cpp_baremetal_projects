2437c417578dc4ab0bd202404f82f1269c7d1fdd
nekosanteam <1688092+nekosanteam@users.noreply.github.com>
2023-02-28 01:25:20 +0900

    add.

--- platforms/05_Libraries/src/nkqueue.cpp
+++ platforms/05_Libraries/src/nkqueue.cpp
@@ -10,81 +10,83 @@
 #include <cstdint>
 #include <cstddef>
 
-#undef NDEBUG
-#include <cassert>
-
 using std::array;
 using std::copy_n;
 
+using nkq_size = size_t;
+
 struct vring {
     std::array<uint8_t, 128> ring;
-    size_t wp;
-    size_t rp;
+    nkq_size wp;
+    nkq_size rp;
 };
 
 bool is_empty(const struct vring& r)
 {
-    size_t rp = r.rp;
-    size_t wp = r.wp;
+    nkq_size rp = r.rp;
+    nkq_size wp = r.wp;
 
     return (rp == wp);
 }
 
-size_t get_room(const struct vring& r)
+nkq_size ring_size(const struct vring& r)
 {
-    size_t rp = r.rp;
-    size_t wp = r.wp;
-    assert((rp < 2*r.ring.size()));
-    assert((wp < 2*r.ring.size()));
+    return (nkq_size)(r.ring.size());
+}
+
+nkq_size get_room(const struct vring& r)
+{
+    nkq_size rp = r.rp;
+    nkq_size wp = r.wp;
 
     if (rp == wp) {
-        return r.ring.size();
+        return ring_size(r);
     }
     if (rp < wp) {
-        assert(((rp + r.ring.size()) >= wp));
-        return ((rp + r.ring.size()) - wp);
+        //assert(((wp - rp) <= r.ring.size()))
+        return (ring_size(r) - (wp - rp));
     }
     else {
-        assert((rp >= (wp + r.ring.size())));
-        return (rp - r.ring.size() - wp);
+        //assert(((rp - wp) >= r.ring.size()))
+        return ((rp - wp) - ring_size(r));
     }
 }
 
-size_t recv(struct vring& r, uint8_t* buf, size_t len)
+nkq_size recv(struct vring& r, uint8_t* buf, size_t len)
 {
     if (is_empty(r)) {
         return 0;
     }
 
-    size_t ret;
-    if ((ret = (r.ring.size() - get_room(r))) < len) {
+    nkq_size ret;
+    if ((ret = (ring_size(r) - get_room(r))) < len) {
         len = ret;
     }
 
-    size_t rp  = r.rp;
-    size_t wp  = r.wp;
+    nkq_size rp  = r.rp;
+    nkq_size wp  = r.wp;
 
-    size_t rrp;
-    if (rp < r.ring.size()) {
+    nkq_size rrp;
+    if (rp < ring_size(r)) {
         rrp = rp;
     }
     else {
-        rrp = rp - r.ring.size();
+        rrp = rp - ring_size(r);
     }
 
-    if ((rp + len) < 2 * r.ring.size()) {
+    if ((rp + len) < 2 * ring_size(r)) {
         rp = rp + len;
     }
     else {
-        assert(rp >= r.ring.size());
-        rp = ((rp - r.ring.size()) + len) - r.ring.size();
+        //assert(rp >= r.ring.size());
+        rp = ((rp - ring_size(r)) + len) - ring_size(r);
     }
 
-    if ((r.ring.size() - rrp) >= len) {
+    if ((ring_size(r) - rrp) >= len) {
         copy_n(&r.ring[rrp], len, &buf[0]);
     }
     else {
-        size_t a = r.ring.size() - rrp;
+        size_t a = ring_size(r) - rrp;
         size_t b = len - a;
         copy_n(&r.ring[rrp], a, &buf[0]);
         copy_n(&r.ring[0], b, &buf[a]);
@@ -109,26 +111,26 @@ size_t send(struct vring& r, const uint8_t* buf, size_t len)
     size_t rp  = r.rp;
 
     size_t rwp;
-    if (wp < r.ring.size()) {
+    if (wp < ring_size(r)) {
         rwp = wp;
     }
     else {
-        rwp = wp - r.ring.size();
+        rwp = wp - ring_size(r);
     }
 
-    if ((wp + len) < 2 * r.ring.size()) {
+    if ((wp + len) < 2 * ring_size(r)) {
         wp = wp + len;
     }
     else {
-        assert(wp >= r.ring.size());
-        wp = ((wp - r.ring.size()) + len) - r.ring.size();
+        //assert(wp >= r.ring.size());
+        wp = ((wp - ring_size(r)) + len) - ring_size(r);
     }
 
-    if ((r.ring.size() - rwp) >= len) {
+    if ((ring_size(r) - rwp) >= len) {
         copy_n(&buf[0], len, &r.ring[rwp]);
     }
     else {
-        size_t a = r.ring.size() - rwp;;
+        size_t a = ring_size(r) - rwp;;
         size_t b = len - a;
         copy_n(&buf[0], a, &r.ring[rwp]);
         copy_n(&buf[a], b, &r.ring[0]);
@@ -140,6 +142,27 @@ size_t send(struct vring& r, const uint8_t* buf, size_t len)
 
 #include <cstdio>
 #include <cstring>
+
+#undef NDEBUG
+#include <cassert>
+
+void assert_vring(struct vring& r)
+{
+    assert(get_room(r) <= r.ring.size());
+    assert(r.wp < 2*r.ring.size());
+    assert(r.rp < 2*r.ring.size());
+
+    if (r.rp == r.wp) {
+        // empty(), ok.
+        return;
+    }
+    else if (r.rp < r.wp) {
+        assert(((r.wp - r.rp) <= r.ring.size()));
+    }
+    else { // rp > wp
+        assert(((r.rp - r.wp) >= r.ring.size()));
+    }
+}
 
 void dump_buffer(uint8_t* buf, int len)
 {
@@ -164,9 +187,6 @@ void print_vring(struct vring& r)
     std::printf("rp = %ld\n", r.rp);
     std::printf("empty = %d, get_room = %ld\n", is_empty(r), get_room(r));
     std::printf("\n");
-    assert(get_room(r) <= r.ring.size());
-    assert(r.wp < 2*r.ring.size());
-    assert(r.rp < 2*r.ring.size());
 }
 
 extern "C"
@@ -186,10 +206,14 @@ int nkqueue_test(int argc, char** argv)
     std::printf("t0\n");
     for (size_t i=0; i<256; i++) {
         send(ring, bufA, 128);
+        assert_vring(ring);
         recv(ring, bufB, 128);
+        assert_vring(ring);
         if ((t0 = std::memcmp(bufA, bufB, 128)) != 0) {
             std::printf("memcmp[%d] = %d\n", i, t0);
             print_vring(ring);
+            assert_vring(ring);
+            return 1;
         }
     }
 
@@ -197,10 +221,14 @@ int nkqueue_test(int argc, char** argv)
     std::printf("t1\n");
     for (size_t i=0; i<256; i++) {
         send(ring, bufA, 1);
+        assert_vring(ring);
         recv(ring, bufB, 1);
+        assert_vring(ring);
         if ((t1 = std::memcmp(bufA, bufB, 1)) != 0) {
             std::printf("memcmp[%d] = %d\n", i, t1);
             print_vring(ring);
+            assert_vring(ring);
+            return 1;
         }
     }
  
@@ -209,13 +237,16 @@ int nkqueue_test(int argc, char** argv)
     for (size_t i=0; i<256; i++) {
         for (size_t j=0; j<128; j++) {
             send(ring, bufA, j+1);
+            assert_vring(ring);
             //print_vring(ring);
             recv(ring, bufB, j+1);
+            assert_vring(ring);
             //print_vring(ring);
             if ((t2 = std::memcmp(bufA, bufB, j+1)) != 0) {
                 dump_buffer(bufB, j+1);
                 std::printf("memcmp[%d, %d] = %d\n", i, j, t2);
                 print_vring(ring);
+                assert_vring(ring);
                 return 1;
             }
             //std::printf("check %4d, %4d, get_room(%4d), rp %4ld, wp %4ld\n", i, j, get_room(ring), ring.rp, ring.wp);
@@ -227,11 +258,14 @@ int nkqueue_test(int argc, char** argv)
     for (size_t i=0; i<256; i++) {
         for (size_t j=128; j>0; j--) {
             send(ring, bufA, j);
+            assert_vring(ring);
             recv(ring, bufB, j);
+            assert_vring(ring);
             if ((t3 = std::memcmp(bufA, bufB, j)) != 0) {
                 dump_buffer(bufB, j);
                 std::printf("memcmp[%d, %d] = %d\n", i, j, t3);
                 print_vring(ring);
+                assert_vring(ring);
                 return 1;
             }
         }
