b082b14f7a0b68a8bb966da9b665f57e1e43c9ec
nekosanteam <1688092+nekosanteam@users.noreply.github.com>
2023-03-02 01:31:23 +0900

    update nkfdtlib.

--- platforms/05_Libraries/export/nkfdtlib.h
+++ platforms/05_Libraries/export/nkfdtlib.h
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: CC0-1.0
 /**
  * @file nkfdtlib.h
- *
  */
 #ifndef NKFDTLIB_H_
 #define NKFDTLIB_H_
@@ -24,16 +23,29 @@ extern "C" {
 typedef int      nkfdt_error;
 typedef uint32_t nkfdt_tag;
 
+enum {
+    NKFDT_OK = 0,
+    NKFDT_ERROR,
+};
+
+#define NKFDT_BEGIN_NODE 0x01
+#define NKFDT_END_NODE   0x02
+#define NKFDT_PROP       0x03
+#define NKFDT_NOP        0x04
+#define NKFDT_END        0x09
+
 struct fdt_header;
 
 nkfdt_error nkfdt_parse_header(const void* fdt, size_t *nextoffset, struct fdt_header* h);
 
 void* nkfdt_skip_node(const void* fdt, size_t* nextoffset, const struct fdt_header* h);
 void* nkfdt_skip_prop(const void* fdt, size_t* nextoffset, const struct fdt_header* h);
-char* nkfdt_get_string(const void* fdt, size_t offset, size_t* strlenp, struct fdt_header* h);
+char* nkfdt_get_string(const void* fdt, size_t str_off, int* strlenp, struct fdt_header* h);
 
 nkfdt_error nkfdt_get_prop_data(const void* fdt, size_t offset, void* buf, size_t* lenp);
 nkfdt_error nkfdt_set_prop_data(const void* fdt, size_t offset, const void* buf, size_t len);
+
+void* nks_memcpy(void* dst, const void* src, size_t n);
 
 #ifdef __cplusplus
 } // extern "C"
--- platforms/05_Libraries/src/nkfdtlib.cpp
+++ platforms/05_Libraries/src/nkfdtlib.cpp
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: CC0-1.0
+/**
+ * @file nkfdtlib.cpp
+ */
 /* reference: https://www.devicetree.org/specification/ [v0.3, v0.4-rc1] */
 #include "nkfdtlib.h"
 
@@ -8,14 +12,14 @@ using std::uint8_t;
 using std::uint32_t;
 using std::uintptr_t;
 
+void* memcpy(void* dst, const void* src, size_t n) __attribute__((weak, alias ("nks_memcpy")));
+
 int nks_memcmp(const void* data1, const void* data2, size_t n)
 {
     const uint8_t* r1 = (const uint8_t*)data1;
     const uint8_t* r2 = (const uint8_t*)data2;
     
-    size_t i;
-
-    for (i=0; i<n; i++) {
+    for (size_t i=0; i<n; i++) {
         if (*r1 != *r2) {
             if (*r1 < *r2) {
                 return -1;
@@ -35,9 +39,8 @@ void* nks_memcpy(void* dst, const void* src, size_t n)
 {
     uint8_t* p1 = (uint8_t*)dst;
     const uint8_t* p2 = (const uint8_t*)src;
-    size_t i;
 
-    for (i=0; i<n; i++) {
+    for (size_t i=0; i<n; i++) {
         *p1 = *p2;
         p1++;
         p2++;
@@ -70,9 +73,9 @@ static uint32_t read32(const void* p)
     uint32_t r = 0;
 
     r = (uint32_t)(*((uint8_t*)p));
-    r = (r <<  8) | (uint32_t)(*(((uint8_t*)p) + 1));
-    r = (r << 16) | (uint32_t)(*(((uint8_t*)p) + 2));
-    r = (r << 24) | (uint32_t)(*(((uint8_t*)p) + 3));
+    r = (r << 8) | (uint32_t)(*(((uint8_t*)p) + 1));
+    r = (r << 8) | (uint32_t)(*(((uint8_t*)p) + 2));
+    r = (r << 8) | (uint32_t)(*(((uint8_t*)p) + 3));
     return r;
 }
 
@@ -89,9 +92,14 @@ static void write32(const void* p, uint32_t v)
     *(((uint8_t*)p) + 3) = (uint8_t)((v >> 24) & 0xFF);
 }
 
-static void* offset_ptr(const void* fdt, size_t offset)
+static inline void* offset_ptr(const void* fdt, size_t offset)
 {
     return (void*)(((const uint8_t*)fdt) + offset);
+}
+
+static inline size_t round4(size_t s)
+{
+    return ((s + 3) & ~0x03u);
 }
 
 nkfdt_tag nkfdt_get_tag(const void* fdt, size_t offset)
@@ -99,18 +107,7 @@ nkfdt_tag nkfdt_get_tag(const void* fdt, size_t offset)
     return read32(offset_ptr(fdt, offset));
 }
 
-enum {
-    NKFDT_OK = 0,
-    NKFDT_ERROR,
-};
-
 #define UNUSED(v) do { (void)v; } while (0)
-
-#define NKFDT_BEGIN_NODE 0x01
-#define NKFDT_END_NODE   0x02
-#define NKFDT_PROP       0x03
-#define NKFDT_NOP        0x04
-#define NKFDT_END        0x09
 
 struct fdt_header {
     uint32_t totalsize;
@@ -126,7 +123,7 @@ struct fdt_header {
 
 nkfdt_error nkfdt_parse_header(const void* fdt, size_t *nextoffset, struct fdt_header* h)
 {
-    size_t offset;
+    size_t   offset;
     uint32_t version;
     uint32_t last_comp_version;
 
@@ -149,7 +146,7 @@ nkfdt_error nkfdt_parse_header(const void* fdt, size_t *nextoffset, struct fdt_h
     offset += 4;
     // version;
     version = read32(offset_ptr(fdt, offset));
-    if (h) { h->totalsize = version; }
+    if (h) { h->version = version; }
     offset += 4;
     // last_comp_version.
     last_comp_version = read32(offset_ptr(fdt, offset));
@@ -178,16 +175,12 @@ nkfdt_error nkfdt_parse_header(const void* fdt, size_t *nextoffset, struct fdt_h
     return NKFDT_OK;
 }
 
-static inline size_t round4(size_t s)
-{
-    return ((s + 3) & ~0x03u);
-}
-
 void* nkfdt_skip_node(const void* fdt, size_t* nextoffset, const struct fdt_header* h)
 {
-    uint32_t tag;
-    size_t len;
-    size_t offset;
+    nkfdt_tag tag;
+    size_t    len;
+    size_t    offset;
+    int       level;
 
     if (nextoffset == NULL) {
         return NULL;
@@ -196,28 +189,22 @@ void* nkfdt_skip_node(const void* fdt, size_t* nextoffset, const struct fdt_head
         return NULL;
     }
     offset = *nextoffset;
-
-    tag = read32(offset_ptr(fdt, offset));
-    if (tag != NKFDT_BEGIN_NODE) {
-        return NULL;
-    }
-    offset += 4;
-    len = nks_strnlen((char*)(offset_ptr(fdt, offset)), h->size_dt_struct);
-    len = round4(len+1);
-    offset += len;
+    level = 0;
 
     do {
         tag = read32(offset_ptr(fdt, offset));
         switch (tag) {
         case NKFDT_BEGIN_NODE:
+            offset += 4;
+            ++level;
+            len = nks_strnlen((char*)(offset_ptr(fdt, offset)), h->size_dt_struct);
+            offset += round4(len+1);
             *nextoffset = offset;
-            (void)nkfdt_skip_node(fdt, nextoffset, h);
-            offset = *nextoffset;
             break;
 
         case NKFDT_END_NODE:
-        case NKFDT_END:
             offset += 4;
+            --level;
             *nextoffset = offset;
             break;
 
@@ -231,18 +218,23 @@ void* nkfdt_skip_node(const void* fdt, size_t* nextoffset, const struct fdt_head
             (void)nkfdt_skip_prop(fdt, nextoffset, h);
             offset = *nextoffset;
             break;
+
+        case NKFDT_END:
+            *nextoffset = offset;
+            break;
+
         }
-    } while ((tag != NKFDT_END_NODE) && (tag != NKFDT_END));
+    } while ((level != 0) && (tag != NKFDT_END));
 
     return offset_ptr(fdt, offset);
 }
 
 void* nkfdt_skip_prop(const void* fdt, size_t* nextoffset, const struct fdt_header* h)
 {
-    uint32_t tag;
-    uint32_t len;
-    size_t   str_off;
-    size_t   offset;
+    nkfdt_tag tag;
+    uint32_t  len;
+    size_t    str_off;
+    size_t    offset;
 
     if (nextoffset == NULL) {
         return NULL;
@@ -268,17 +260,82 @@ void* nkfdt_skip_prop(const void* fdt, size_t* nextoffset, const struct fdt_head
     return offset_ptr(fdt, offset);
 }
 
-char* nkfdt_get_string(const void* fdt, size_t offset, int* strlenp, struct fdt_header* h)
+char* nkfdt_get_string(const void* fdt, size_t str_off, int* strlenp, struct fdt_header* h)
 {
+    char* str;
 
+    str = (char*)offset_ptr(fdt, h->off_dt_strings + str_off);
+    if (str == NULL) {
+        return NULL;
+    }
+
+    if (strlenp) {
+        size_t len;
+        len = nks_strnlen(str, h->size_dt_strings);
+        *strlenp = len;
+    }
+
+    return str;
 }
 
-nkfdt_error nkfdt_get_prop_data(const void* fdt, size_t offset, void** bufp, int* lenp)
+int nkfdt_match_node(const void* fdt, size_t* offset, const char* node_name, struct fdt_header* h)
 {
+    size_t len;
 
+    len = nks_strnlen(node_name, h->size_dt_struct) + 1;
+    if (nks_memcmp(node_name, (char*)offset_ptr(fdt, *offset + 4), len) != 0) {
+        return 0;
+    }
+    *offset += 4;
+    *offset += round4(len);
+    return 1;
 }
 
-nkfdt_error nkfdt_set_prop_data(const void* fdt, size_t offset, const void* buf, int len)
+int nkfdt_match_prop(const void* fdt, size_t* offset, const char* prop_name, struct fdt_header* h)
 {
+    size_t match_len;
+    size_t prop_len;
+    size_t str_off;
+    char*  prop_str;
 
+    match_len = nks_strnlen(prop_name, h->size_dt_strings) + 1;
+
+    prop_len = read32(offset_ptr(fdt, *offset + 4));
+    str_off  = read32(offset_ptr(fdt, *offset + 8));
+    prop_str = nkfdt_get_string(fdt, str_off, NULL, h);
+    if (prop_str == NULL) {
+        return 0;
+    }
+    if (nks_memcmp(prop_name, prop_str, match_len) != 0) {
+        return 0;
+    }
+
+    return 1;
+}
+
+nkfdt_error nkfdt_get_prop_data(const void* fdt, size_t offset, void* buf, size_t* lenp)
+{
+    size_t prop_len;
+
+    prop_len = read32(offset_ptr(fdt, offset + 4));
+    if ((lenp == NULL) || (*lenp < prop_len)) {
+        return NKFDT_ERROR;
+    }
+    nks_memcpy(buf, offset_ptr(fdt, offset + 12), prop_len);
+    *lenp = prop_len;
+
+    return NKFDT_OK;
+}
+
+nkfdt_error nkfdt_set_prop_data(const void* fdt, size_t offset, const void* buf, size_t len)
+{
+    size_t prop_len;
+
+    prop_len = read32(offset_ptr(fdt, offset + 4));
+    if (len != prop_len) {
+        return NKFDT_ERROR;
+    }
+    nks_memcpy(offset_ptr(fdt, offset + 12), buf, prop_len);
+
+    return NKFDT_OK;
 }
