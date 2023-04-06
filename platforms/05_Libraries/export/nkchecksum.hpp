1450451407c7530fe5e01ef2d8bc0791e58e481e
nekosanteam <1688092+nekosanteam@users.noreply.github.com>
2023-04-07 01:18:53 +0900

    wip.

--- platforms/05_Libraries/export/nkchecksum.hpp
+++ platforms/05_Libraries/export/nkchecksum.hpp
@@ -1,13 +1,29 @@
 /**
- * @file nkhash.hpp
+ * @file nkchecksum.hpp
  */
-#ifndef NKHASH_HPP_
-#define NKHASH_HPP_
+#ifndef NKCHECKSUM_HPP_
+#define NKCHECKSUM_HPP_
 
 #include <cstdint>
 
 namespace bm {
-namespace hash {
+namespace checksum {
+
+class checksum {
+public:
+	checksum() : value_(0) {}
+	static void update(std::uint8_t* buf, std::size_t offset, std::size_t len);
+
+public:
+	std::uint_least8_t value_;
+};
+
+void checksum::update(std::uint8_t* buf, std::size_t offset, std::size_t len)
+{
+	for (std::size_t i=offset; i<(offset+len); i++) {
+		value_ = value_ + buf[i]
+	}
+}
 
 template <std::uint16_t p, std::uint16_t ini, bool ri, bool ro, std::uint16_t xo>
 class crc16 {
@@ -39,4 +55,4 @@ std::uint16_t nk_crc16C(std::size_t count, const std::uint8_t* buffer);
 }
 }
 
-#endif /* NKHASH_HPP_ */
+#endif /* NKCHECKSUM_HPP_ */
